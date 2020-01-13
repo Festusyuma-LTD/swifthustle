27e8202c0faf2c25337f6a39f5d3ddb843bd475d
unknown <dafom.chollom@longbridgetech.com>
2020-01-13 10:00:32 +0100

    home navbar and text pending images

---
+++ components/navbars/AppHomeNavBar.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="">
+    <div class="home-navbar">
+      <b-navbar class="navbar" toggleable="lg">
+        <div class="container-fluid">
+          <b-navbar-brand href="#">
+            SpinMill
+          </b-navbar-brand>
+
+          <b-navbar-toggle target="nav-collapse" />
+
+          <b-collapse id="nav-collapse" is-nav>
+            <!-- <b-navbar-nav>
+              <b-nav-item href="#">
+                Link
+              </b-nav-item>
+              <b-nav-item href="#" disabled>
+                Disabled
+              </b-nav-item>
+            </b-navbar-nav> -->
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <!-- <b-nav-form>
+                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+              </b-nav-form> -->
+              <b-nav-item href="/registerPage">
+                <b-button id="btn-one">
+                  Register
+                </b-button>
+              </b-nav-item>
+              <b-nav-item href="/userHomePage">
+                <b-button id="btn-two" size="sm">
+                  Sign in
+                </b-button>
+              </b-nav-item>
+            </b-navbar-nav>
+          </b-collapse>
+        </div>
+      </b-navbar>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeNavbar'
+}
+</script>
+
+<style lang="scss" scoped>
+.home-navbar{
+  .navbar{
+    background: transparent;
+  }
+  #btn-one{
+    background: #FFD009;
+    color: #282828;
+    padding:10px 30px;
+    font-size:20px;
+    font-weight: 600;
+    border-radius: 40px;
+    border:2px solid #FFD009;
+    box-shadow: 3px 4px 15px .5px #aca6a6;
+    margin: 0px 0px;
+  }
+  #btn-two{
+    background: transparent;
+    color: #FFD009;
+    border:2px solid #FFD009;
+    padding:10px 30px;
+    margin: 0px 0px 0px 20px;
+    font-size:20px;
+    font-weight: 600;
+    border-radius: 40px;
+    box-shadow: 3px 4px 15px .5px #aca6a6;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,67 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        shproject-front
-      </h1>
-      <h2 class="subtitle">
-        My finest Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="">
+    <homeNavbar />
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-5">
+          <div class="content">
+            <h1 id="intro-statement">
+              Get <br><b>1000%</b><br>On registration
+            </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+            <b-button id="join-btn" size="lg">
+              Join us
+            </b-button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+// import Logo from '~/components/Logo.vue'
+import homeNavbar from '@/components/navbars/AppHomeNavBar.vue'
 export default {
   components: {
-    Logo
+    // Logo
+    homeNavbar
   }
 }
 </script>
 
-<style>
-.container {
+<style lang="scss" scoped>
+.content{
+  #intro-statement{
+    color: #f8f8f8;
+    font-size: 60px;
+    b{
+      color: #FFD009;
+      font-size: 165px;
+    }
+  }
+  #join-btn{
+    background: #FFD009;
+    color: #282828;
+    padding:8px 50px;
+    font-size:35px;
+    font-weight: 600;
+    border-radius: 40px;
+    border:2px solid #FFD009;
+    box-shadow: 3px 4px 15px .5px #aca6a6;
+    margin: 0px 0px;
+    transition: all 0.5s;
+    -webkit-transition: all 0.5s;
+    transition-timing-function: ease;
+    -webkit-transition-timing-function: ease;
+  }
+  #join-btn:hover{
+    // transition: all 0.5s;
+    // -webkit-transition: all 0.5s;
+    transform: scale(1.08);
+  }
+}
+/* .container {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
@@ -68,5 +90,5 @@ export default {
 
 .links {
   padding-top: 15px;
-}
+} */
 </style>
